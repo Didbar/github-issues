61c89fc45ed5abfcbaaa47cf25ae3c962c5a8145
Daviti Didbaridze <ddidbaridze@gmail.com>
2023-02-02 16:37:42 +0100

    center tabs for small devices

--- src/components/IssueListFilter.tsx
+++ src/components/IssueListFilter.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Tabs, Tab } from '@mui/material'
 
 interface Props {
@@ -8,8 +8,15 @@ interface Props {
 }
 const IssueListFilter: React.FC<Props> = ({ state, onChange, statesCounts }) => {
   const { closedCount, openedCount } = statesCounts.repository
+  const [smallScreen, setSmallScreen] = useState(window.matchMedia('(min-width: 768px)').matches)
+  useEffect(() => {
+    window
+      .matchMedia('(max-width: 800px)')
+      .addEventListener('change', (e) => setSmallScreen(e.matches))
+  }, [])
+
   return (
-    <Tabs value={state} onChange={onChange}>
+    <Tabs value={state} onChange={onChange} centered={smallScreen}>
       <Tab value='OPEN' label={`OPEN (${openedCount.totalCount})`} />
       <Tab value='CLOSED' label={`CLOSED (${closedCount.totalCount})`} />
     </Tabs>
